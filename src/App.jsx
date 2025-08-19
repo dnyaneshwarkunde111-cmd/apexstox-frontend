5046700c6f177df03b45a11a79dd571843c88b51
dnyaneshwarkunde111-cmd <dnyaneshwarkunde111@gmail.com>
2025-08-19 13:33:49 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,32 @@
 import React, { useState } from 'react';
 import LoginPage from './LoginPage.jsx';
-import SignupPage from './SignupPage.jsx'; // Import the new Signup Page
+import SignupPage from './SignupPage.jsx';
+import Dashboard from './Dashboard.jsx'; // Import the new Dashboard
 import './index.css';
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false); // Manages if user is logged in
   const [isLoginPage, setIsLoginPage] = useState(true);
 
-  // This function will toggle between login and signup pages
+  // This function will be called from LoginPage on successful login
+  const handleLoginSuccess = () => {
+    setIsLoggedIn(true);
+  };
+
   const togglePage = () => {
     setIsLoginPage(!isLoginPage);
   };
 
-  return (
-    <div>
-      {isLoginPage ? <LoginPage onSwitch={togglePage} /> : <SignupPage onSwitch={togglePage} />}
-    </div>
-  );
+  // If user is logged in, show Dashboard. Otherwise, show Login/Signup.
+  if (isLoggedIn) {
+    return <Dashboard />;
+  } else {
+    return (
+      <div>
+        {isLoginPage ? <LoginPage onSwitch={togglePage} onLoginSuccess={handleLoginSuccess} /> : <SignupPage onSwitch={togglePage} />}
+      </div>
+    );
+  }
 }
 
 export default App;
-
